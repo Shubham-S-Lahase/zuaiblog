1b541d8df9dd1e801bd2e005882a014cb1761b2e
Shubham Lahase <shubhamlahase@gmail.com>
2024-08-21 19:02:41 +0530

    Register Form Submit Button Animation added

--- client/src/components/register/Register.jsx
+++ client/src/components/register/Register.jsx
@@ -1,10 +1,23 @@
-import styles from "./Register.module.css"
+import styles from "./Register.module.css";
+import { useState } from "react";
 
 const RegisterForm = () => {
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!isChecked) {
+      setIsChecked(true);
+      setTimeout(() => {
+        setIsChecked(false);
+      }, 8000);
+    }
+  };
+
   return (
     <div className={styles.registercontainer}>
       <h2 className={styles.registertitle}>Create an Account</h2>
-      <form className={styles.registerform}>
+      <form className={styles.registerform} onSubmit={handleSubmit}>
         <div className={styles.formgroup}>
           <label htmlFor="username">Username</label>
           <input
@@ -45,9 +58,20 @@ const RegisterForm = () => {
           />
         </div>
 
-        <button type="submit" className={styles.registerbutton}>
-          Register
-        </button>
+        <div id="button-wrapper" className={`${styles.buttonWrapper} ${isChecked ? styles.checked : ''}`}>
+          <button type="submit" className={`${styles.registerbutton} ${styles.submit}`}>Register</button>
+          <div className={styles.loaderWrapper}>
+            <span className={`${styles.loader} ${styles.yellow}`}></span>
+            <span className={`${styles.loader} ${styles.pink}`}></span>
+          </div>
+          <span className={`${styles.loader} ${styles.orange}`}></span>
+
+          <div className={styles.checkWrapper}>
+            <svg version="1.1" width="65px" height="38px" viewBox="0 0 64.5 37.4">
+              <polyline className={styles.check} points="5,13 21.8,32.4 59.5,5 " />
+            </svg>
+          </div>
+        </div>
       </form>
     </div>
   );
