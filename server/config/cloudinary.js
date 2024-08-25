7b2ba4a73e06c601c25756645489df0fc3e56789
Shubham Lahase <shubhamlahase@gmail.com>
2024-08-25 10:48:55 +0530

    Removed server folder from client branch

--- server/config/cloudinary.js
+++
@@ -1,9 +0,0 @@
-const cloudinary = require("cloudinary").v2;
-
-cloudinary.config({
-  cloud_name: process.env.CLD_NAME,
-  api_key: process.env.CLD_API_KEY,
-  api_secret: process.env.CLD_SECRET,
-});
-
-module.exports = cloudinary;
